039d9542606b45b662bc4b8a51e9211ac213dd15
dextermcmillan <dexmcmillan@gmail.com>
2021-04-26 09:38:44 -0400

    changed library used.

--- regression.py
+++ regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import scipy.stats
 from sklearn.impute import SimpleImputer
 
 import matplotlib.pyplot as plt  # To visualize
@@ -18,18 +18,17 @@ for col in range(20):
     measure = data.columns[3+col]
     X = data.iloc[:, 2].values.reshape(-1, 1)  # values converts it into a numpy array
     Y = data.iloc[:, 3+col].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
-
+    print(data.columns[2])
     # (this handles any blank or N/A values in the data.)
     imputer = SimpleImputer()
     Y = imputer.fit_transform(Y)
 
-    # ...then do a linear regression...
-    linear_regressor = LinearRegression()
-    reg = linear_regressor.fit(X.astype(float), Y.astype(float))
-    score = reg.score(X, Y)
+    result = scipy.stats.linregress(X[:,0], Y[:,0])
+    r_value = result.rvalue
+    p_value = result.pvalue
 
     # ..and finally put into a new dataframe that will be concatenated together at the end.
-    row = pd.DataFrame([[measure, score]])
+    row = pd.DataFrame([[measure, '{:f}'.format(r_value), '{:f}'.format(p_value)]])
     parts.append(row)
 
 # Put everything in our parts list together into a dataframe for export.
