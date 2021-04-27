89af732de896d45cc3fbe0be1aba153721589805
dextermcmillan <dexmcmillan@gmail.com>
2021-04-26 21:06:08 -0400

    moved file.

--- analysis/ontario.py
+++ analysis/ontario.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Read in data. This set is not included in github repo because it is too large.
 # Download from https://www12.statcan.gc.ca/census-recensement/2016/dp-pd/prof/details/page_Download-Telecharger.cfm?Lang=E&Tab=1&Geo1=PR&Code1=01&Geo2=PR&Code2=01&SearchText=Canada&SearchType=Begins&SearchPR=01&B1=All&TABID=1&type=0.
-data = pd.read_csv("./data/data-all.csv")
+data = pd.read_csv("../data/data-all.csv")
 
 # Filter for Ontario FSAs.
 ontario = (data[
